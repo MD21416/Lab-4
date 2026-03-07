bf7d6179ac1e419aac7986f4d6c3d5dd66d96fae
MD21416 <mespino1@umbc.edu>
2026-03-07 00:33:23 +0000

    yes

--- read_file.c
+++ read_file.c
@@ -3,16 +3,21 @@
 extern int sum(int* array, int size);
 
 int main(){
+    // Prompt user for file name
     char filename[30];
     printf("Which file would you like to sum? ");
     scanf("%s", filename);
+
+    // Attempt to open the file
     FILE * file;
     file = fopen(filename, "r");
     
+    // Check if the file exists
     if(file != NULL){
+        // Find the size of the file (in the first line)
         int size;
         if(fscanf(file, "%d", &size)){
-            
+            // Create an array and store all the numbers
             int arr[size];
             int iterator = 0;
             int num;
@@ -20,18 +25,15 @@ int main(){
                 arr[iterator] = num;
                 iterator += 1;
             }
-
-            int cVal = 0;
-            for(int i = 0; i < size; i++){
-                cVal += arr[i];
-            }
-            printf("Sum from C: %d\n", cVal);
+            
+            // Pass the array of numbers and its size to assembly
             int asmVal = sum(arr, size);
             printf("Sum from Assembly: %d\n", asmVal);
         }
         fclose(file);
     }
     else{
+        // In case file name is mistyped/wrong
         printf("Invalid file name.\n");
     }
     return 0;
--- sum.s
+++ sum.s
@@ -1,21 +1,21 @@
 .section .text
-.globl sum         # Make function visible to C program
+.globl sum                      # Make function visible to C program
 .globl loop
 .globl finish
 
 sum: 
-    mov $0, %eax
-    mov $0, %ecx
+    mov $0, %eax                # Set sum (EAX) to 0
+    mov $0, %ecx                # Set iterator (ECX) to 0
 
 loop:
-    cmpl %esi, %ecx
-    jge finish
+    cmpl %esi, %ecx             # Compare ECX to the size of the array
+    jge finish                  # Finish loop if sum is complete
 
-    addl (%rdi, %rcx, 4), %eax
-    addl $1, %ecx
-    jmp loop
+    addl (%rdi, %rcx, 4), %eax  # Add current array element to sum
+    addl $1, %ecx               # Add 1 to iterator (ECX)
+    jmp loop                    # Go to next array element
 
 finish:        
-    ret                 # Return control back to C program 
+    ret                         # Return control back to C program 
 
 .section .note.GNU-stack,"",@progbits
